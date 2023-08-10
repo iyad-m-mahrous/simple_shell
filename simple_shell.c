4bafe6ea40032b83ed7e9559b6989824348eb47e
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-08-10 06:38:10 -0700

    Adding not istty function for non terminal

--- simple_shell.c
+++ simple_shell.c
@@ -14,7 +14,7 @@
 int main(int argc, char *argv[])
 {
 	char *line = NULL, *args[10];
-	int i, pid, is_exit = 1, is_atty, err_count = 0;
+	int i, pid = -1, is_exit = 1, is_atty, err_count = 0;
 	size_t len = 0;
 	ssize_t line_len;
 
@@ -38,29 +38,26 @@ int main(int argc, char *argv[])
 			printf("%s: %d: %s: not found\n", argv[0], ++err_count, args[0]);
 			free(line);
 			line = NULL;
-			if (!is_atty)
-				return (0);
 			continue;			
 		}
+
 		for (i = 1; i < 10; i++)
 		{
 			args[i] = strtok(NULL, " ");
 			if (args[i] == NULL)
 				break;
 		}
-		if (is_atty)
-			pid = fork();
-		if (pid == 0 || !is_atty)
+		pid = fork();
+		if (pid == 0)
 		{
 			execve(args[0], args, NULL);
 			free(line);
 			line = NULL;
-			return (0);
 		}
 		else
 			wait(NULL);
 	}
-	if (is_exit != 0)
+	if (is_exit != 0 && is_atty)
 		printf("\n");
 	return (0);
 }
