6e4b51f99d9694e5da8a3916810a019497b9b8e5
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-08-10 08:41:34 -0700

    Fixing the trailing space issue

--- Iyad/simple_shell.c
+++ Iyad/simple_shell.c
@@ -11,10 +11,10 @@
  * Return: -1 if error else 0
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *env[])
 {
 	char *line = NULL, *args[10];
-	int i, pid, is_exit = 1, is_atty, err_count = 0;
+	int i, pid = -1, is_exit = 1, is_atty, err_count = 0;
 	size_t len = 0;
 	ssize_t line_len;
 
@@ -30,37 +30,46 @@ int main(int argc, char *argv[])
 			free(line);
 			break;
 		}
+		if (strcmp(line, "env\n") == 0)
+		{
+			while (*env)
+			{
+				printf("%s\n", *env);
+				env++;
+			}
+			free(line);
+			line = NULL;
+			continue;
+		}
 		if (line[line_len - 1] == '\n')
 			line[line_len - 1] = '\0';
 		args[0] = strtok(line, " ");
 		if (access(args[0], X_OK) == -1)
 		{
-			printf("%s: %d: %s: not found\n", argv[0], ++err_count, args[0]);
+			if (args[0])
+				printf("%s: %d: %s: not found\n", argv[0], ++err_count, args[0]);
 			free(line);
 			line = NULL;
-			if (!is_atty)
-				return (0);
 			continue;			
 		}
+
 		for (i = 1; i < 10; i++)
 		{
 			args[i] = strtok(NULL, " ");
 			if (args[i] == NULL)
 				break;
 		}
-		if (is_atty)
-			pid = fork();
-		if (pid == 0 || !is_atty)
+		pid = fork();
+		if (pid == 0)
 		{
 			execve(args[0], args, NULL);
 			free(line);
 			line = NULL;
-			return (0);
 		}
 		else
 			wait(NULL);
 	}
-	if (is_exit != 0)
+	if (is_exit != 0 && is_atty)
 		printf("\n");
 	return (0);
 }
