13fd6d4f1cbe16a43165b88c518bb7162d7526e8
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-08-24 08:25:25 -0700

    passing the env to execve

--- Iyad/env_free.c
+++ Iyad/env_free.c
@@ -11,17 +11,17 @@ void env_free(char *env[])
 {
 	int i = 0;
 
+	while (environ[i] != NULL)
+	{
+		if (((unsigned long)env >> 24) != ((unsigned long)(environ[i]) >> 24))
+		{
+			free(environ[i]);
+			environ[i] = NULL;
+		}
+		i++;
+	}
 	if (environ != env)
 	{
-		while (environ[i] != NULL)
-		{
-			if (((unsigned long)environ >> 24) == ((unsigned long)(environ[i]) >> 24))
-			{
-				free(environ[i]);
-				environ[i] = NULL;
-			}
-			i++;
-		}
 		free(environ);
 		environ = env;
 	}
